5978bf115c19ea8e94ffc7ce20e25ad694d46b22
clabby <ben@clab.by>
2024-02-21 11:47:32 -0700

    Ensure `L1Block` account is loaded into the cache

--- crates/revm/src/optimism/l1block.rs
+++ crates/revm/src/optimism/l1block.rs
@@ -64,6 +64,9 @@ pub struct L1BlockInfo {
 impl L1BlockInfo {
     /// Try to fetch the L1 block info from the database.
     pub fn try_fetch<DB: Database>(db: &mut DB, spec_id: SpecId) -> Result<L1BlockInfo, DB::Error> {
+        // Ensure the L1 Block account is loaded into the cache.
+        let _ = db.basic(L1_BLOCK_CONTRACT)?;
+
         let l1_base_fee = db.storage(L1_BLOCK_CONTRACT, L1_BASE_FEE_SLOT)?;
 
         if !spec_id.is_enabled_in(SpecId::ECOTONE) {
